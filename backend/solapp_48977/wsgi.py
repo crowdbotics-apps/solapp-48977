30051b8a617e95a121ce5932949bcad324cbd5aa
Crowdbotics <team@crowdbotics.com>
2024-08-07 16:37:15 +0000

    Initial commit

---
+++ backend/solapp_48977/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for solapp_48977 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "solapp_48977.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solapp-48977",
+  url: "https://solapp-48977.botics.co",
+  api: "https://solapp-48977.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
